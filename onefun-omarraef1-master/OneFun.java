918aa607e9407281983a9d97f8bc6c80678d77da
Omar Raéf <omarraef1@email.arizona.edu>
2021-04-22 09:15:06 -0400

    added-unit-test

---
+++ onefun-omarraef1-master/OneFun.java
@@ -0,0 +1,27 @@
+/**
+ * This has only one method to make you think about how to compare 
+ * two strings because in Java, we can not write "A" < "B"
+ * 
+ * @author Rick Mercer and Omar R. Gebril
+ */
+public class OneFun {
+
+  // Complete method first to return the "lesser" of 2 strings.
+  //
+  //  first("A", "B") returns "A"
+  //  first("Z", "a") returns "Z"
+  //  first("1", "2") returns "1"
+  //  first("abc", "abc ") returns "abc"
+  //  
+  public String first(String a, String b) {
+	  if (a.compareTo(b) < 0 ) {
+		  return a;}
+	  else if (a.compareTo(b) == 0) {
+		  return a;
+		  }
+	  else {
+		  return b;
+	  }
+		 
+  }
+}
---
+++ onefun-omarraef1-master/OneFunTest.java
@@ -0,0 +1,26 @@
+
+/**
+ * A unit test for one method that will help in the next
+ * project because you need compareTo and compareTo is so weird
+ */
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OneFunTest {
+
+  OneFun oneFun = new OneFun();
+
+  @Test
+  public void testLexocographicallyFirst() {
+    assertEquals("", oneFun.first("", "A"));
+    assertEquals("0", oneFun.first("A", "0"));
+    assertEquals("1", oneFun.first("1", "A"));
+    assertEquals("?", oneFun.first("?", "A"));
+    assertEquals("A", oneFun.first("A", "a"));
+    assertEquals("Z", oneFun.first("Z", "a"));
+    assertEquals("abc", oneFun.first("abc", "abc"));
+    assertEquals("abc", oneFun.first("abc ", "abc"));
+    assertEquals("abc", oneFun.first("abc", "abcd"));
+  }
+}
